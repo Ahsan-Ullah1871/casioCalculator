91929727f654c20b3c3c47396af7bdac80da36d4
Ahsan Ullah <ahsanullahsunsbd@gmail.com>
2021-01-31 14:52:14 +0600

    change

--- calc.js
+++ calc.js
@@ -1,27 +1,52 @@
-
-function clickNum(value) {
-    document.getElementById('displayOn').value += value;
-        document.getElementById("displayOff").value = "";
-
-}  
-
-function clickEqual() {
-     document.getElementById("displayOff").style.display = "block";
-    var calculation = eval(document.getElementById("displayOn").value);
-    document.getElementById("displayOff").value +=  calculation;
-    
-
+function ON() {
+	document.getElementById("displayOn").value = "0";
+	
 }
 
-function clickAc() {
-    document.getElementById("displayOn").value = '';
-    document.getElementById("displayOff").value = '';
-}
-function clickDel() {
-    let allSelect = document.getElementById("displayOn").value;
-    let piece = allSelect.length;
-    let lastNumber = allSelect.slice(0, piece - 1);
-    document.getElementById("displayOn").value = lastNumber;
-    
-}
+document.getElementById("buttons").addEventListener("click", function (event) {
+	let getValue = event.target.value;
 
+	if (isNaN(getValue)) {
+		if (getValue == "=") {
+			let show = document.getElementById("displayOn").value;
+
+			if (show == "") {
+			} else {
+				document.getElementById(
+					"displayOff"
+				).style.display = "block";
+				var calculation = eval(
+					document.getElementById("displayOn").value
+				);
+				document.getElementById(
+					"displayOff"
+				).value = calculation;
+			}
+		} else if (
+			getValue == "+" ||
+			getValue == "-" ||
+			getValue == "*" ||
+			getValue == "." ||
+			getValue == "/"
+		) {
+			document.getElementById("displayOn").value += getValue;
+			document.getElementById("displayOff").value = "";
+		} else if (getValue == "AC") {
+			document.getElementById("displayOn").value = "";
+			document.getElementById("displayOff").value = "";
+		} else if (getValue == "DEL") {
+			let allSelect = document.getElementById("displayOn").value;
+			let piece = allSelect.length;
+			let lastNumber = allSelect.slice(0, piece - 1);
+			document.getElementById("displayOn").value = lastNumber;
+		}
+	} else {
+		let show = document.getElementById("displayOn").value;
+		if (show == "0") {
+			document.getElementById("displayOn").value = getValue;
+		} else {
+			document.getElementById("displayOn").value += getValue;
+			document.getElementById("displayOff").value = "";
+		}
+	}
+});
